4d2fb4f17e47bf01995e5f9fad3f3f7ba8accadf
TSShrol <shrolts@gmail.com>
2024-10-11 20:24:11 +0300

    added IAccountsService, AccountsService, AccountsController

---
+++ WebAPI_ShopTech_PV321.Core/Sevices/AccountsService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebAPI_ShopTech_PV321.Core.DTOs.User;
+using WebAPI_ShopTech_PV321.Core.Interfaces;
+
+namespace WebAPI_ShopTech_PV321.Core.Sevices
+{
+    public class AccountsService : IAccountsService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountsService(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager= userManager;
+            _signInManager= signInManager;
+        }
+        public async Task<IdentityUser> Get(string id)
+        {
+            var user= await _userManager.FindByIdAsync(id);
+            if (user == null) {
+                throw new Exception("User not found by id");
+            }
+            return user;
+        }
+
+        public Task<string> Login(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Register(RegisterDto registerDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebAPI_ShopTech_PV321/Controllers/AccountsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_ShopTech_PV321.Core.Interfaces;
+using WebAPI_ShopTech_PV321.Core.Sevices;
+
+namespace WebAPI_ShopTech_PV321.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountsService _accountsService;
+        public AccountsController(IAccountsService accountsService)
+        {
+            _accountsService = accountsService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            var product = _accountsService.Get(id);
+            return Ok(product);
+        }
+
+
+    }
+}
